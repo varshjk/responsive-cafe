90ec881ae17bfd8dad77fd4496cc640848ecba02
varshjk <139230754+varshjk@users.noreply.github.com>
2023-09-08 23:34:30 +0530

    js, CSS, Html files uploaded

---
+++ index.js
@@ -0,0 +1,23 @@
+let search = document.querySelector('.search-box') ;
+document.querySelector('#search-icon').onclick = ()=>{
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.Navbar') ;
+document.querySelector('#menu-icon').onclick = ()=>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = ()=>{
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
